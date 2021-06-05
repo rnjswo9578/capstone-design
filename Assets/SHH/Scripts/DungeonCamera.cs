5052457e7d1d8138b58a7ce4662e0b5a74d6aabb
rnjswo9578 <80087131+rnjswo9578@users.noreply.github.com>
2021-06-05 23:26:10 +0900

    123

--- Assets/SHH/Scripts/DungeonCamera.cs
+++ Assets/SHH/Scripts/DungeonCamera.cs
@@ -9,16 +9,13 @@ public class DungeonCamera : MonoBehaviour
     public float x, y, z;
     private List<GameObject> mTransparentWalls=new List<GameObject>();
     private List<GameObject> newAddedWall=new List<GameObject>();
-    private float maxdis,maxdis1,maxdis2;
-    private Vector3 pos1,pos2;
+    private float maxdis;
     // Start is called before the first frame update
     void Start()
     {
         if (player == null)
             player = GameObject.FindGameObjectWithTag("PLAYER");
         maxdis = Mathf.Sqrt(x * x + (y-1) * (y-1) + z * z);
-        maxdis1 = Mathf.Sqrt(x * x + (y + 1) * (y + 1) + z * z);
-        maxdis2 = Mathf.Sqrt(x * x + (y - 1) * (y - 1) + (z+5) * (z+5));
 
     }
 
@@ -26,8 +23,6 @@ public class DungeonCamera : MonoBehaviour
     void Update()
     {
         Vector3 offset = new Vector3(x, y, z); //0, 13, -20
-        pos1 =player.transform.position + new Vector3(x, 1, z);
-        pos2 = player.transform.position + new Vector3(x,y,z+5);
         transform.position = player.transform.position + offset;
         transform.LookAt(player.transform);
         FadeOutWall();
@@ -37,9 +32,7 @@ public class DungeonCamera : MonoBehaviour
     {
         Vector3 ScreenPos = Camera.main.WorldToScreenPoint(player.transform.position);
         Ray ray = Camera.main.ScreenPointToRay(ScreenPos);
-        Ray ray2 = new Ray(pos1, ScreenPos);
-        Ray ray3 = new Ray(pos2, ScreenPos);
-        RaycastHit[] hits = Physics.RaycastAll(ray, maxdis);// Physics.RaycastAll(ray2,maxdis1)+Physics.RaycastAll(ray3,maxdis2);
+        RaycastHit[] hits = Physics.RaycastAll(ray,maxdis);
         newAddedWall.Clear();
         foreach (RaycastHit hit in hits)
         {
