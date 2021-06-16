8871fda366a809de22e126adceaed79e92aedf06
rnjswo9578 <80087131+rnjswo9578@users.noreply.github.com>
2021-06-16 22:49:24 +0900

    123124

--- Assets/LJI/Scrpits/Status/lji_statusManager.cs
+++ Assets/LJI/Scrpits/Status/lji_statusManager.cs
@@ -128,32 +128,6 @@ public class lji_statusManager : MonoBehaviour
         leftWeaponTier[0] = other;
         GameObject.FindWithTag("PLAYER").SendMessage("GetPlayerStatus");
     }
-<<<<<<< Updated upstream
-    
-=======
-
-    void LoadXml(string filename)
-    {
-        TextAsset textAsset = (TextAsset)Resources.Load(filename);
-        XmlDocument xmlDoc = new XmlDocument();
-        xmlDoc.LoadXml(textAsset.text);
-
-        XmlNode goldXml = xmlDoc.SelectSingleNode("GoldInfo/Gold");
-        gold=int.Parse(goldXml.InnerText);
-
-    }
-
-    public void OverwriteXml()
-    {
-        TextAsset textAsset = (TextAsset)Resources.Load("Gold");
-        XmlDocument xmlDoc = new XmlDocument();
-        xmlDoc.LoadXml(textAsset.text);
-
-        XmlNode goldXml = xmlDoc.SelectSingleNode("GoldInfo/Gold");
-        goldXml.InnerText = gold + "";
-
-        xmlDoc.Save("./Assets/Resources/Gold.xml");
-    }
 
     public void changeSubW(int r, int l)
     {
@@ -166,7 +140,5 @@ public class lji_statusManager : MonoBehaviour
         rightWeaponTier[1] = other;
         leftWeaponTier[1] = other;
         GameObject.FindWithTag("PLAYER").SendMessage("GetPlayerStatus");
-
     }
->>>>>>> Stashed changes
 }
